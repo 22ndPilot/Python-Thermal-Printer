a4538fb66f503871fe0d855b4f1e99f9890cceb1
maddiarmstrong7 <61703643+maddiarmstrong7@users.noreply.github.com>
2020-04-29 17:28:31 -0500

    Update quote.py

--- quote.py
+++ quote.py
@@ -14,19 +14,7 @@ import random
 
 
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
-  def getFileContent("22ndPilot/Python-Thermal-Printer/blob/master/story1.txt"):
-    with open("22ndPilot/Python-Thermal-Printer/blob/master/story1.txt", 'r') as theFile:
-        # Return a list of lines (strings)
-        # data = theFile.read().split('\n')
-        
-        # Return as string without line breaks
-        # data = theFile.read().replace('\n', '')
-        
-        # Return as string
-        data = theFile.read()
-        return data
-
-printer.print getFileContent('./story1.txt')
+printer.print("story1.txt")
 
 
 printer.feed(3)
