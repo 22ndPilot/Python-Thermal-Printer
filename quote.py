ac3b746c6efff7f35cec82d75359b3623a5cec20
maddiarmstrong7 <61703643+maddiarmstrong7@users.noreply.github.com>
2020-04-29 17:16:07 -0500

    Update quote.py

--- quote.py
+++ quote.py
@@ -10,12 +10,24 @@ import lxml as lh
 import os
 import random
 
-
+url = "https://github.com/22ndPilot/Python-Thermal-Printer/blob/master/test/story15"
+page = urllib.urlopen(url)
+doc = str(page.read())
+index = doc.find("<p class=\"# start">")
+doc = doc[index:-1]
+index = doc.find("<p>")
+doc = doc[index+3:-1]
+end = doc.find("<p class=\"# end">")
+result = doc[:end].replace("</p>", "").replace("\\t",
+    "").replace("\\n","").replace("\\r", "").replace("<p>",
+        "\n").replace("&nbsp;"," ").replace("&ldquo;","\"").replace("&rdquo;",
+            "\"").split("<br />")
 
 
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
+for x in result:
+    printer.print(x)
+    printer.println(' ')
 
-
-printer.print('imgay')
 
 printer.feed(3)
