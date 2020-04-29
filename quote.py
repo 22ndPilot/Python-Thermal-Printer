02e771efde60ec4b15ffa7086bd8ed29c5b7e25f
maddiarmstrong7 <61703643+maddiarmstrong7@users.noreply.github.com>
2020-04-29 17:28:03 -0500

    Update quote.py

--- quote.py
+++ quote.py
@@ -26,7 +26,7 @@ printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
         data = theFile.read()
         return data
 
-printer.print getFileContent('./test.txt')
+printer.print getFileContent('./story1.txt')
 
 
 printer.feed(3)
