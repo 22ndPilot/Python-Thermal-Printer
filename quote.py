2a3e2041e2b517fdcb4b2ef728597735ed2418c3
maddiarmstrong7 <61703643+maddiarmstrong7@users.noreply.github.com>
2020-04-29 16:07:44 -0500

    Update quote.py

--- quote.py
+++ quote.py
@@ -7,33 +7,17 @@ from Adafruit_Thermal import *
 
 from urllib2 import urlopen
 import lxml as lh
-import random
-
-import sys
 import os
 import random
+	
+	
 
-path = sys.argv[1]
-
-def randomize_file():
-	return random.choice(os.listdir("test"))
-
-def print_random_filename(random_file):
-	random_filename = random_file.split("")[0]
-	print random_filename
-
-random_file = random.choice(os.listdir("test"))
-
-if random_file.startswith('.'):
-	new_random_file = randomize_file()
-	print_random_filename(new_random_file)
-else:
-	print_random_filename(random_file)
 
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 for x in result:
-    printer.print(x)
+ x = printer.print(random.choice(list(open('story[n+1]'))))
     printer.println(' ')
+	
 
 
 printer.feed(3)
