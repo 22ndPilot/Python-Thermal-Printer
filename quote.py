b03d80e3dcc6bf52931583feaaf700bf2d0bf450
maddiarmstrong7 <61703643+maddiarmstrong7@users.noreply.github.com>
2020-04-29 18:34:14 -0500

    Update quote.py

--- quote.py
+++ quote.py
@@ -18,7 +18,7 @@ import uuid
 
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
-listOfFiles = ['story1.txt', 'story2.txt', 'story3.txt']
+listOfFiles = ['story(int(n)).txt']
 selectedFile = random.choice(listOfFiles)
 f = open(selectedFile, 'r')
 file_contents = f.read()
