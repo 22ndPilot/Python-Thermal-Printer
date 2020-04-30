341b8827f80e4db9d10f5516cd1c10bdd053a54f
maddiarmstrong7 <61703643+maddiarmstrong7@users.noreply.github.com>
2020-04-29 19:10:55 -0500

    Update quote.py

--- quote.py
+++ quote.py
@@ -20,7 +20,7 @@ printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
 i = [1, 50]
 listOfFiles = ['story1.txt', 'story2.txt', 'story3.txt', 'story4.txt', 'story5.txt', 'story6.txt', 'story7.txt', 'story8.txt',
-               'story8.txt', 'story9.txt']
+               'story8.txt', 'story9.txt', 'story10.txt']
 
 selectedFile = random.choice(listOfFiles)
 f = open(selectedFile, 'r')
