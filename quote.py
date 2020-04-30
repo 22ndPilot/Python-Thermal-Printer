678f1bfcffc3a9873d97b1776dcb90715d303351
maddiarmstrong7 <61703643+maddiarmstrong7@users.noreply.github.com>
2020-04-29 19:20:22 -0500

    Update quote.py

--- quote.py
+++ quote.py
@@ -25,7 +25,7 @@ listOfFiles = ['story1.txt', 'story2.txt', 'story3.txt', 'story4.txt', 'story5.t
               'story24.txt', 'story25.txt', 'story26.txt', 'story27.txt', 'story28.txt', 'story29.txt', 'story30.txt', 'story31.txt'
               'story32.txt', 'story33.txt', 'story34.txt', 'story35.txt', 'story36.txt', 'story37.txt', 'story38.txt', 'story39.txt'
               'story40.txt', 'story41.txt', 'story42.txt', 'story43.txt', 'story44.txt', 'story45.txt', 'story46.txt', 'story47.txt'
-              'story48.txt', 'story49.txt', 'story50.txt', 'story51.txt]
+              'story48.txt', 'story49.txt', 'story50.txt', 'story51.txt']
 
 selectedFile = random.choice(listOfFiles)
 f = open(selectedFile, 'r')
